a1c5675e9bbd40488d9658a0a8c2c992d95c9024
Terayama <terayas@909c4ad0144b.ant.amazon.com>
2022-06-09 19:50:00 +0900

    amplify.yml 1

--- script.sh
+++ script.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-aws ssm put-parameter --cli-input-json '{ "Name": "/amplify/appurl", "Value": "https://${USER_BRANCH}.${AWS_APP_ID}.amplifyapp.com", "Type": "String"}'
+aws ssm put-parameter --cli-input-json '{ "Name": "/amplify/appurl", "Value": "https://${AWS_BRANCH}.${AWS_APP_ID}.amplifyapp.com", "Type": "String"}'
