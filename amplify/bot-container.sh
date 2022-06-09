05fde3583578931c4a0fb9f0c7f408b89c5b57f4
Terayama <terayas@909c4ad0144b.ant.amazon.com>
2022-06-10 00:16:34 +0900

    iam ecr docker

--- amplify/bot-container.sh
+++ amplify/bot-container.sh
@@ -3,4 +3,8 @@
 cd clics-bot-container
 
 AWS_ACCOUNT=`aws ssm get-parameter --name "/amplify/account"|jq -r ".Parameter.Value"`
+ECR_REPOSITORY_NAME=`aws ssm get-parameter --name "/amplify/ecr_name"|jq -r ".Parameter.Value"`
 aws ecr get-login-password --region ${AWS_Region} | docker login --username AWS --password-stdin ${AWS_ACCOUNT}.dkr.ecr.${AWS_Region}.amazonaws.com
+docker build -t bot .
+docker tag bot ${AWS_ACCOUNT}.dkr.ecr.${AWS_Region}.amazonaws.com/${ECR_REPOSITORY_NAME}:latest
+docker push ${AWS_ACCOUNT}.dkr.ecr.${AWS_Region}.amazonaws.com/${ECR_REPOSITORY_NAME}:latest
