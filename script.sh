9e78b8d1f05331346008a12852f06f7435f53fa4
Terayama <terayas@909c4ad0144b.ant.amazon.com>
2022-06-09 21:11:49 +0900

    graphqlurl

--- script.sh
+++ script.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-aws ssm put-parameter --cli-input-json '{ "Name": "/amplify/appurl", "Value": "https://${AWS_BRANCH}.${AWS_APP_ID}.amplifyapp.com", "Type": "String"}'
+aws ssm put-parameter --cli-input-json '{ "Name": "/amplify/app_url", "Value": "https://${AWS_BRANCH}.${AWS_APP_ID}.amplifyapp.com", "Type": "String"}'
+APIURL=`aws appsync get-graphql-api --api-id `aws ssm get-parameter --name "/amplify/appsyncid"|jq -r ".Parameter.Value"`|jq -r ".graphqlApi.uris.GRAPHQL"`
+aws ssm put-parameter --cli-input-json '{ "Name": "/amplify/graphqlurl", "Value: "${APIURL}", "Type":"String" }' 
