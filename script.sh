fcf0748bec528791ae8f334a74234e954e5d7922
Terayama <terayas@909c4ad0144b.ant.amazon.com>
2022-06-09 22:51:03 +0900

    set overwirte parameter

--- script.sh
+++ script.sh
@@ -3,7 +3,7 @@
 
 aws configure set cli_follow_urlparam false
 # Put Amplify App URL to SSM Parameter at app_url
-aws ssm put-parameter --name "/amplify/app_url" --value "https://${AWS_BRANCH}.${AWS_APP_ID}.amplifyapp.com" --type "String"
+aws ssm put-parameter --name "/amplify/app_url" --value "https://${AWS_BRANCH}.${AWS_APP_ID}.amplifyapp.com" --type "String" --overwrite
 
 API_ID=`aws ssm get-parameter --name "/amplify/appsyncid"|jq -r ".Parameter.Value"`
 
@@ -12,4 +12,7 @@ APIURL=`aws appsync get-graphql-api --api-id ${API_ID}|jq -r ".graphqlApi.uris.G
 
 
 # Put Graphql API URI to SSM Param at /amplify/graphqlurl
-aws ssm put-parameter --name "/amplify/graphql_api" --value "${APIURL}" --type "String" 
+aws ssm put-parameter --name "/amplify/graphql_api" --value "${APIURL}" --type "String" --overwrite
+
+# Put DynamoDB Table to SSM Param at /amplify/tablename
+aws ssm put-parameter --name "/amplify/tablename" --value `aws appsync get-data-source --api-id ${API_ID} --name TodoTable|jq -r ".dataSource.dynamodbConfig.tableName"` --type "String" --overwrite
