b4657735c12f4996f3316a848dbca335f5af39db
Terayama <terayas@909c4ad0144b.ant.amazon.com>
2022-06-09 19:18:06 +0900

    amplify.yml put amplify_url

--- script.sh
+++ script.sh
@@ -2,5 +2,5 @@
 
 echo $REGION
 echo $AWS_BRANCH
-aws ssm put-parameter --name /amplify/appid --value $AWS_APP_ID --type String
+echo $ACCOUNT
 aws ssm put-parameter --name /amplify/backendappid --value $AMPLIFY_BACKEND_APP_ID --type String
